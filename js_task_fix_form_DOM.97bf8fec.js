8204276078f3ffdde9334a5e7b6848be4d0e4c69
Bohdan Diatliuk <dyatlyuk.bohdan@gmail.com>
2025-10-05 17:20:47 +0300

    make build

---
+++ js_task_fix_form_DOM.97bf8fec.js
@@ -0,0 +1,2 @@
+"use strict";var forms=document.querySelectorAll("form");forms.forEach(function(e){e.querySelectorAll("input").forEach(function(e){var t=document.createElement("label");t.classList.add("field-label"),t.setAttribute("for",e.id),t.textContent=e.name,e.parentElement.insertBefore(t,e),e.placeholder=e.name.charAt(0).toUpperCase()+e.name.slice(1)})});
+//# sourceMappingURL=js_task_fix_form_DOM.97bf8fec.js.map
---
+++ js_task_fix_form_DOM.f75de5e1.js
@@ -0,0 +1,15 @@
+'use strict';
+const forms = document.querySelectorAll('form');
+forms.forEach((form)=>{
+    const inputs = form.querySelectorAll('input');
+    inputs.forEach((input)=>{
+        const label = document.createElement('label');
+        label.classList.add('field-label');
+        label.setAttribute('for', input.id);
+        label.textContent = input.name;
+        input.parentElement.insertBefore(label, input);
+        input.placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+    });
+});
+
+//# sourceMappingURL=js_task_fix_form_DOM.f75de5e1.js.map
